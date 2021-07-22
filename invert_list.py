80fd174dff8c5998260fa91f712d037fa66d5730
Abeygail Panganiban <abeygailpanganiban@Abeygails-MacBook-Pro.local>
2021-07-22 15:16:26 -0700

    cleaned up code

--- invert_list.py
+++ invert_list.py
@@ -9,13 +9,13 @@ input_str = input_str[0].replace(", ", " ").split()
 
 
 def reverse_func(words):
-		words.reverse()
-		rev_str = []
-		for word in words:
-			w = len(word)
-			slicedstr = word[w::-1]
-			rev_str.append(slicedstr)
-		print(rev_str)
+	words.reverse()
+	reversed_str = []
+	for word in words:
+		word_len = len(word)
+		slicedstr = word[word_len::-1]
+		reversed_str.append(slicedstr)
+	print(reversed_str)
 
 reverse_func(input_str)
 
