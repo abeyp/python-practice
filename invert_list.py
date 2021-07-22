56ad9ca7e54e2853734ce227e421c2256b904dea
stephone <stephone_christian@apple.com>
2021-07-22 13:20:27 -0700

    adding practice questions

---
+++ invert_list.py
@@ -0,0 +1,10 @@
+#given a list of strings, return a list with all of the elements in reverse order, and reverse the spelling of each string element
+#ex: ["fizz", "buzz"] -> ["zzub", "zzif"]
+
+
+
+input_str = ['Revelling, maths, decolor, hangout, smectic, Nicola, terrorizer, ruthenic, precede, unequalness, Ketoxime, addictedness, eyeservant, disproof, roentgenologically, Unacoustical, hypnotizing, bay, unecstatic, preoceanic, Mightiest, intersesamoid, dross, bigotry, especialness']
+
+
+def reverse_func():
+	pass
---
+++ prime_numbers.py
@@ -0,0 +1,9 @@
+#find all the prime numbers in the range of [0-1000]
+#use online resources to verify your answers
+#nothing fancy, use a brute force apporach! :) 
+
+
+def find_prime_numbers():
+	pass
+
+
