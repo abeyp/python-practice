29050a0927875d30e691037aedaa1875eb7d4a6f
Abeygail Panganiban <abeygailpanganiban@Abeygails-MacBook-Pro.local>
2021-07-22 15:13:57 -0700

    finished problem

--- invert_list.py
+++ invert_list.py
@@ -8,9 +8,15 @@ input_str = ['Revelling, maths, decolor, hangout, smectic, Nicola, terrorizer, r
 input_str = input_str[0].replace(", ", " ").split()
 
 
+def reverse_func(words):
+		words.reverse()
+		rev_str = []
+		for word in words:
+			w = len(word)
+			slicedstr = word[w::-1]
+			rev_str.append(slicedstr)
+		print(rev_str)
 
-def reverse_func():
-	pass
+reverse_func(input_str)
 
-for word in reversed(input_str):
-	print(word)
+
